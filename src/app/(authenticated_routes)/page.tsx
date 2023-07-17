f79fdb8a215433e7b16f1c44300e5667ad4825cc
Rodri Marchese <rodrimarchese@gmail.com>
2023-07-17 15:41:46 -0300

    updates

--- src/app/(authenticated_routes)/page.tsx
+++ src/app/(authenticated_routes)/page.tsx
@@ -1,11 +1,27 @@
+"use client";
 import React from "react";
 import UserData from "@/components/userData";
 import FetchButton from "@/components/fetchButton";
+import { Box, Typography } from "@mui/material";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const Home = () => {
-  return (
-      <UserData/>
-  )
-}
+  const containerStyles = {
+    width: "100vw",
+    backgroundColor: "#f5f5f5",
+    padding: "20px",
+  };
 
-export default Home
+  const itemStyles = {
+    marginBottom: "10px",
+  };
+  return (
+    <Box sx={containerStyles}>
+      <Typography color="black" variant="h3" sx={itemStyles}>
+        Welcome to snippets searcher!
+      </Typography>
+    </Box>
+  );
+};
+
+export default Home;
